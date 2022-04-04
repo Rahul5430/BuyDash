721867abf0be724a5f28ed70a9378afe45e9691f
Rahul5430 <rahul213himani@gmail.com>
2022-04-04 22:57:05 +0530

    initial commit

---
+++ src/data/index.js
@@ -0,0 +1,232 @@
+export const data = [
+    {
+        "createdAt": "Wed Nov 10 2021",
+        "name": "bottle",
+        "category": "Tablets",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fleaf.jpg?alt=media&token=b044ab77-eccc-444f-85d7-650e4ff8465f",
+        "price": "2000",
+        "userId": "m6NFORQXFfg5aJ0cQiWOcFuToez1",
+        "description": "1 time use",
+        "id": "IXP0nQn2LVAevWmJdrOX"
+    },
+    {
+        "createdAt": "Tue Oct 19 2021",
+        "price": "22899",
+        "userId": "FLVKLVevqxMs5qwncqCzNARaPyc2",
+        "category": "Tablets",
+        "description": "In-boxed S Pen with revamped Ergonomic design Superior display with breathtaking sound Signature Galaxy Experience with One UI & Samsung Kids",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fin-galaxy-tab-s6-lite-p610-sm-p610nzaainu-dynamicgray-236966866.webp?alt=media&token=bac5f07d-fd27-4633-9862-54a645c9a1c0",
+        "name": "Galaxy Tab S6",
+        "id": "qCjK6qfjeP84NlNWQz7B"
+    },
+    {
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2FIMG-20211023-WA0002.jpg?alt=media&token=13d55833-8752-4ec0-bae1-566945c6c259",
+        "price": "62000",
+        "category": "Motorcycles",
+        "createdAt": "Sat Oct 30 2021",
+        "description": "Good vehicle 20.k running km",
+        "userId": "tM7GGDGqDHR6tDm0oUOizPMmA6f2",
+        "name": "Dio",
+        "id": "z8ZlXPC9a3ZzRBOJjNFO"
+    },
+    {
+        "name": "iphone 11 256 gb",
+        "createdAt": "Sat Oct 09 2021",
+        "price": "45000",
+        "category": "Mobile Phones",
+        "userId": "ANTsV5ja27W4Ym3nktxMjXxzsYu1",
+        "description": "Liquid Retina HD display · 15.5 cm / 6.1‑inch (diagonal) all-screen LCD Multi-Touch display with IPS technology",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fimage.jpeg?alt=media&token=199c3d96-1557-4792-83ac-29d94abd6611",
+        "id": "sO6Qsdb1mDIFiQ6g6SgY"
+    },
+    {
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fa24d53e3e6d0356ebc681f15d27231c8.jpg?alt=media&token=fa3609cd-240f-4e02-8a58-a421c4eecd8d",
+        "description": "Less used .. ONAM offer.. great mileage around 5km..",
+        "userId": "GLDzj1StSuQB7yZi6kS83JOrTfM2",
+        "category": "Motorcycles",
+        "price": "480000",
+        "createdAt": "Sat Oct 09 2021",
+        "name": "Kawasaki",
+        "id": "ii7jM0k5E7kXDuKxbwhM"
+    },
+    {
+        "description": "BMW i8 is a lovely vehicle to drive even though it’s lacking. It hugs the road and commands attention. It’s thrilling in a way that few cars can achieve without speed. Sure, it’s quick, but it won’t set track records or quarter-mile times.",
+        "category": "Cars",
+        "createdAt": "Sat Oct 09 2021",
+        "price": "859000",
+        "userId": "GLDzj1StSuQB7yZi6kS83JOrTfM2",
+        "name": "BMW i8",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fdownload%20(4).jpeg?alt=media&token=f693d472-85bf-47b6-97dc-37c8b1eb37b4",
+        "id": "g4kYmJsCPp5wUvZcVIW9"
+    },
+    {
+        "description": "Create detailed DSLR quality pictures and cinematic Full HD movies with ease, even in difficult low light situations, using the 24.1 Megapixel EOS 2000D",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fdownload%20(3).jpeg?alt=media&token=40a42278-1228-47a8-b1a6-3df2226cc9ab",
+        "name": "Canon 200 D ",
+        "price": "52499",
+        "category": "Cameras & Lenses",
+        "createdAt": "Sat Oct 09 2021",
+        "userId": "AKf07OwwtZNxF7PELpEhbly7CV63",
+        "id": "QHhcfbonVDqCYFnWU9kx"
+    },
+    {
+        "category": "Cameras & Lenses",
+        "price": "58000",
+        "name": "Nikon D5600",
+        "createdAt": "Sat Oct 09 2021",
+        "userId": "ANTsV5ja27W4Ym3nktxMjXxzsYu1",
+        "description": "Nikon's EXPEED image processor combined with an outstanding autofocus system and the ability to shoot at ISO 25,600",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fdownload%20(2).jpeg?alt=media&token=7a405b35-7328-4065-9c2a-6c08bb89159d",
+        "id": "NmXsHLJHPUNHWr68aJUz"
+    },
+    {
+        "userId": "GLDzj1StSuQB7yZi6kS83JOrTfM2",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fdownload%20(6).jpeg?alt=media&token=d80babc3-1188-4786-82f4-c0a3e3336d34",
+        "name": "ipad pro 2020",
+        "price": "62899",
+        "category": "Tablets",
+        "description": "fresh sealed box ..",
+        "createdAt": "Sat Oct 09 2021",
+        "id": "DZiirQJCND7fWc4ozs5T"
+    },
+    {
+        "createdAt": "Sat Oct 09 2021",
+        "category": "Mobile Phones",
+        "price": "18999",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fdownload%20(1).jpeg?alt=media&token=7d4ede09-e8db-4b70-98a1-3b0600acc5d3",
+        "description": " 188g, 7.3mm thickness · iOS 10.0.1, up to iOS 15 ",
+        "userId": "AKf07OwwtZNxF7PELpEhbly7CV63",
+        "name": "iphone 7 plus 64 gb",
+        "id": "7ibMWIOg3bWNE5EpACxw"
+    },
+    {
+        "price": "92499",
+        "category": "Computers & Laptops",
+        "userId": "GLDzj1StSuQB7yZi6kS83JOrTfM2",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fdownload%20(5).jpeg?alt=media&token=b4481453-ac53-41aa-a4f8-3604beb94bbe",
+        "createdAt": "Sat Oct 09 2021",
+        "description": "The 16” MacBook Pro comes configured with either a 2.6GHz 6-core i7 or a 2.3GHz 8-core i9 from Intel. These are the same processors as the 15” MacBook Pro came with. No advancements here is largely a function of Intel’s chip readiness. ",
+        "name": "Mac book 512 gb",
+        "id": "12Hmf8P889bN8uPOjpL1"
+    },
+    {
+        "userId": "ANTsV5ja27W4Ym3nktxMjXxzsYu1",
+        "category": "Cameras & Lenses",
+        "price": "48000",
+        "name": "Nikon D5300",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fimages%20(5).jpeg?alt=media&token=40e772b9-ddb4-4729-be60-65aeafc1684b",
+        "description": "Nikon makes outstanding image quality possible with the perfect combination of technological advances: a newly developed Nikon DX-format CMOS image sensor, Nikon's exclusive new EXPEED 4 image-processing engine, Picture Control, and the uncompromising optical quality of NIKKOR lenses.",
+        "createdAt": "Mon Oct 11 2021",
+        "id": "rWNfwELgOmJWpCcE6vuA"
+    },
+    {
+        "description": " Every angle of the Honda Amaze has been shaped to make a bigger Impression.The aggressive front, the Imposing shape,the sculpted ",
+        "price": "459999",
+        "category": "Cars",
+        "name": "Honda Amaze",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fdownload%20(10).jpeg?alt=media&token=041288da-3fb7-43cc-90ac-dfba7cbf194c",
+        "createdAt": "Mon Oct 11 2021",
+        "userId": "YEbWhn25aKep0FGkWHAmLb71mcX2",
+        "id": "n0ATubmVfsqL6jP4dp5t"
+    },
+    {
+        "category": "Motorcycles",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fimages%20(2).jpeg?alt=media&token=b403502e-c30f-484d-bb71-611d02e64469",
+        "price": "98000",
+        "name": "R15 v3 28000km running",
+        "userId": "LlMJsPTckPOWg366PMmSlrWYobG2",
+        "description": "mint condition.. second owner .,price slightly negotiable",
+        "createdAt": "Mon Oct 11 2021",
+        "id": "l1OQleZ7d7qY89lYoZIZ"
+    },
+    {
+        "description": "The 4th generation model of Canon's best-selling full-frame EOS camera EOS 5D Mark IV comes equipped with 30.4megapixel full-frame CMOS sensor and highly ...",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fimages%20(4).jpeg?alt=media&token=823596d7-e84b-48b6-abd7-555e8600ac85",
+        "userId": "ANTsV5ja27W4Ym3nktxMjXxzsYu1",
+        "name": "Mark 4 5d body only ",
+        "category": "Cameras & Lenses",
+        "price": "86499",
+        "createdAt": "Mon Oct 11 2021",
+        "id": "hyW2nBsax3pELlspCsBN"
+    },
+    {
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fimages%20(3).jpeg?alt=media&token=9d43086c-7e1e-4d61-aa1f-fca5a5bce526",
+        "createdAt": "Mon Oct 11 2021",
+        "price": "72899",
+        "name": "ThinkPad L14 512GB",
+        "userId": "GLDzj1StSuQB7yZi6kS83JOrTfM2",
+        "description": "The ThinkPad L14 (AMD) laptop makes it easy to stay connected to what matters thanks to its new WiFi 6 and optional WWAN capability*. From faster server access to uninterrupted video streaming, this laptop’s super connectivity ensures more productive uptime and more enjoyable downtime anywhere you go.",
+        "category": "Computers & Laptops",
+        "id": "bcz8FPLWnKNi6Yayg5pG"
+    },
+    {
+        "category": "Cars",
+        "name": "Maruthi suzuki 800",
+        "createdAt": "Mon Oct 11 2021",
+        "userId": "YEbWhn25aKep0FGkWHAmLb71mcX2",
+        "description": "300000 km done... 4 th owner .. good condition.. great mileage .. family car..",
+        "price": "48000",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fdownload%20(9).jpeg?alt=media&token=b98ba025-da2e-4047-ba7f-42bc734538fc",
+        "id": "VM6i4vzoDIvUhBaC1d0l"
+    },
+    {
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fimages.jpeg?alt=media&token=04e18015-9f7b-45d8-9b0b-5c9ce1c47a30",
+        "category": "Mobile Phones",
+        "name": "iphone 11 256gb",
+        "createdAt": "Mon Oct 11 2021",
+        "description": " Liquid Retina HD display · 15.5 cm / 6.1‑inch (diagonal) all-screen LCD Multi-Touch display with IPS technology ",
+        "userId": "uGW8ejbUd8cukC3NlmiGgx8AaOT2",
+        "price": "32899",
+        "id": "MxLT2ttM1hjZReUJMmTI"
+    },
+    {
+        "price": "18499",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fdownload%20(7).jpeg?alt=media&token=4453739a-5f88-40f8-987a-ce0345147416",
+        "createdAt": "Mon Oct 11 2021",
+        "userId": "pkVEQCVKDHMUdcyQOMbS95g6t9i1",
+        "category": "Mobile Phones",
+        "name": "Redmi note 10 pro max 128 gb",
+        "description": "REDMI Note 10 Pro Max (Vintage Bronze, 128 GB) (6 GB RAM) · 6 GB RAM | 128 GB ROM | Expandable Upto 512 GB · 16.94 cm (6.67 inch) Full HD+ Display · 108MP ",
+        "id": "FUIy6XGmnqFWUyafxWRD"
+    },
+    {
+        "createdAt": "Mon Oct 11 2021",
+        "description": "KTM Duke 250 single owner.. no bargaining direct call..mint condition..first service is done..not a single scratch..reason for selling : I am going to uk in next month.. urgent sale..",
+        "category": "Motorcycles",
+        "name": "Duke 250 7000km ",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2F2020-ktm-250-duke-left-side-eac4.png?alt=media&token=e3b3a938-c57b-4255-95b1-80d54dcf5a93",
+        "userId": "TCdvtfaSpiPqrjRaqhgXvzy7ecq1",
+        "price": "235999",
+        "id": "4wxNqjoNHCMI66uLr3Pd"
+    },
+    {
+        "userId": "uGW8ejbUd8cukC3NlmiGgx8AaOT2",
+        "price": "26899",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fimages%20(1).jpeg?alt=media&token=62659e18-a342-4b99-8482-d6e6184fcac9",
+        "name": "iphone 11 64 gb",
+        "category": "Mobile Phones",
+        "createdAt": "Mon Oct 11 2021",
+        "description": " Liquid Retina HD display · 15.5 cm / 6.1‑inch (diagonal) all-screen LCD Multi-Touch display with IPS technology ·",
+        "id": "0XzYUwqzbcpGcdBitV7k"
+    },
+    {
+        "name": "Iphone 11 pro 128gb",
+        "description": "Triple-lens cameras w/ new ultra wide-angle lens. More durable, water resistant body. Matte finish and new dark green color. Night Mode for better low-light images. Haptic Touch instead of 3D Touch. Ultra Wideband support. A13 chip. Faster WiFi and LTE.",
+        "price": "68000",
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Faiphone11-pro-camera-cs-1-768244-1602497450.png?alt=media&token=c3f72795-c813-4830-828e-8bc478021b04",
+        "category": "Mobile Phones",
+        "userId": "AKf07OwwtZNxF7PELpEhbly7CV63",
+        "createdAt": "Mon Oct 04 2021",
+        "id": "Y3pRWWStGbL349FnGYPN"
+    },
+    {
+        "url": "https://firebasestorage.googleapis.com/v0/b/olx-sijeesh.appspot.com/o/image%2Fsuzuki%20mehran.jpg?alt=media&token=6b490fe6-7313-42ae-b4b9-61b5cbdd58ed",
+        "createdAt": "Fri Nov 05 2021",
+        "price": "40000",
+        "name": "Suuzuki",
+        "category": "Cars",
+        "description": "Suzuki Mehran 2021",
+        "userId": "vjqsVT4JNDWMZj8GOpwOI89XUmi1",
+        "id": "xsm2HZnJQrUfuYEEwjTH"
+    }
+]
